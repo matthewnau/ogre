886c0ef4e116817f0a9a79c3ebc79a02a120bc22
Matthew Nau <matthewnau@users.noreply.github.com>
2018-06-10 23:58:36 -0400

    designed ui
    added a nice ui and implemented editing functionality

--- main.js
+++ main.js
@@ -6,11 +6,43 @@
     
     target.innerHTML = html;
 }*/
+
+var editor = document.getElementsByClassName("editor-wrapper")[0];
+var body = document.getElementsByTagName("body")[0];
+var textarea = document.getElementsByTagName("textarea")[0];
+var preview = document.getElementsByClassName("markdown-body")[0];
+var editButton = document.getElementsByClassName("editButton")[0];
+var previewButton = document.getElementsByClassName("previewButton")[0];
+
 function run () {
-    var text = document.getElementById('sourceTA').value;
+    var text = textarea.value;
     target = document.getElementById('targetDiv');
     
     target.innerHTML = marked(text);
 }
 
-document.getElementsByTagName("button")[0].onclick = function () {run()};
+function close () {
+    editor.classList.add("hidden");
+    preview.classList.remove("hidden");
+    editButton.classList.remove("hidden");
+    previewButton.classList.add("hidden");
+}
+
+function toggle () {
+    if (editor.classList.contains("hidden")) {
+        open();
+    }
+    else {
+        close();
+    }
+}
+
+function open () {
+    editor.classList.remove("hidden");
+    preview.classList.add("hidden");
+    editButton.classList.add("hidden");
+    previewButton.classList.remove("hidden");
+}
+
+document.getElementsByTagName("button")[0].onclick = function () {toggle()};
+textarea.onkeyup = function () {run()};
